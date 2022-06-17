71406e2df9908a3749807ee72769b8db0e4e5d4b
romerodevelopments <gandresrp@gmail.com>
2022-06-17 16:27:36 -0400

    primeros pasos del proyecto - directiva apply de tailwind

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -6,13 +6,15 @@ const contactForm = () => (
       id='contact-content'
       name="contact-content"
       htmlFor=""
-      className='block text-gray-700 text-sm font-bold mb-2'>Texto por definir</label>
+      className='block text-gray-700 text-sm font-bold mb-2'>
+        Cuéntame de esa idea que quieres realizar
+      </label>
     <div className='flex rounded bg-white border p-2'>
       <textarea
         id='contact-content'
         name="contact-content"
         className='flex-1 py-3 px-3 text-gray-700 focus:outline-none focus:shadow-md'></textarea>
-      <button className='btn'>Enviar</button>
+      <button className='btn ml-4'>Enviar</button>
     </div>
 
   </form>
